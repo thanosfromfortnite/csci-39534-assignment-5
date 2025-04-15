2f70a059ee855aa84045dec7a95785b4b1e87954
thanosfromfortnite <jhan3537@bths.edu>
2025-04-15 16:41:16 -0400

    progress part 2

--- lab5.py
+++ lab5.py
@@ -6,7 +6,7 @@
 """
 from PIL import Image
 
-output_dir = "output/"
+output_dir = "images/"
 
 def bitplane_decomposition(image):
     img = image.convert('L')
@@ -25,5 +25,15 @@ def bitplane_decomposition(image):
 
         out.save(f"{output_dir}{k}.png")
 
+def bitplane_recomposition(image_files, width, height, lsb=4):
+    out = Image.new(mode='L', size=(width, height))
+    out_pixels = out.load()
+    
+
+# Image Bitplane Decomposition
 image = Image.open('dog.png')
 bitplane_decomposition(image)
+
+# Noise Removal in OCT Image using Bitplane Decomposition
+bitplanes = [f"{output_dir}{str(i)}.png" for i in range(8)]
+bitplane_recomposition(bitplanes, image.size[0], image.size[1])
